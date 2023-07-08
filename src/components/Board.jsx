3da5873f3f4b57e8126e022fb3e26839b7b1622c
Thomas Nikoloff <tomnikoloff@hotmail.co.uk>
2023-07-08 11:07:32 +0100

    Cards added. Assets

---
+++ src/components/Board.jsx
@@ -0,0 +1,149 @@
+import lolaOne from '../assets/images/lolaOne.jpg';
+import lolaTwo from '../assets/images/lolaTwo.jpg';
+import lolaThree from '../assets/images/lolaThree.jpg';
+import lolaFour from '../assets/images/lolaFour.jpg';
+import lolaFive from '../assets/images/lolaFive.jpg';
+import lolaSix from '../assets/images/lolaSix.jpg';
+import lolaSeven from '../assets/images/lolaSeven.jpg';
+import lolaEight from '../assets/images/lolaEight.jpg';
+import lolaNine from '../assets/images/lolaNine.jpg';
+import lolaTen from '../assets/images/lolaTen.jpg';
+import lolaEleven from '../assets/images/lolaEleven.jpg';
+import lolaTwelve from '../assets/images/lolaTwelve.jpg';
+
+import logo from '../assets/images/logo.png';
+
+export default function Board(){
+    
+    return (
+        <>
+            <div uk-grid className="uk-grid uk-child-width-1-4@m uk-child-width-1-6@l">
+                <div>
+                    <div className="card-cover">
+                        <div className='card-logo-overlay'>
+                            <div>
+                                <img src={logo}/>
+                            </div>
+                        </div>
+                        <img className='card' src={lolaOne}/>
+                    </div>
+                </div>
+                <div>
+                    <div className="card-cover">
+                        <img src=""/>
+                    </div>
+                </div>
+                <div>
+                    <div className="card-cover">
+                        <img src=""/>
+                    </div>
+                </div>
+                <div>
+                    <div className="card-cover">
+                        <img src=""/>
+                    </div>
+                </div>
+                <div>
+                    <div className="card-cover">
+                        <img src=""/>
+                    </div>
+                </div>
+                <div>
+                    <div className="card-cover">
+                        <img src=""/>
+                    </div>
+                </div>
+                <div>
+                    <div className="card-cover">
+                        <img src=""/>
+                    </div>
+                </div>
+                <div>
+                    <div className="card-cover">
+                        <img src=""/>
+                    </div>
+                </div>
+                <div>
+                    <div className="card-cover">
+                        <img src=""/>
+                    </div>
+                </div>
+                <div>
+                    <div className="card-cover">
+                        <img src=""/>
+                    </div>
+                </div>
+                <div>
+                    <div className="card-cover">
+                        <img src=""/>
+                    </div>
+                </div>
+                <div>
+                    <div className="card-cover">
+                        <img src=""/>
+                    </div>
+                </div>
+                <div>
+                    <div className="card-cover">
+                        <img src=""/>
+                    </div>
+                </div>
+                <div>
+                    <div className="card-cover">
+                        <img src=""/>
+                    </div>
+                </div>
+                <div>
+                    <div className="card-cover">
+                        <img src=""/>
+                    </div>
+                </div>
+                <div>
+                    <div className="card-cover">
+                        <img src=""/>
+                    </div>
+                </div>
+                <div>
+                    <div className="card-cover">
+                        <img src=""/>
+                    </div>
+                </div>
+                <div>
+                    <div className="card-cover">
+                        <img src=""/>
+                    </div>
+                </div>
+                <div>
+                    <div className="card-cover">
+                        <img src=""/>
+                    </div>
+                </div>
+                <div>
+                    <div className="card-cover">
+                        <img src=""/>
+                    </div>
+                </div>
+                <div>
+                    <div className="card-cover">
+                        <img src=""/>
+                    </div>
+                </div>
+                <div>
+                    <div className="card-cover">
+                        <img src=""/>
+                    </div>
+                </div>
+                <div>
+                    <div className="card-cover">
+                        <img src=""/>
+                    </div>
+                </div>
+                <div>
+                    <div className="card-cover">
+                        <img src=""/>
+                    </div>
+                </div>
+            </div>  
+        </>
+    )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import logo from '../assets/images/logo.png';
+
+export default function Header() {
+  return (
+    <>
+      <div className="header">
+        <img src={logo} alt="Logo" height={100} />
+        <div className='uk-text-center'>
+            <h1>Memory Game</h1>
+            <h2>Click on two cards to see if they match.</h2>
+        </div>
+      </div>
+    </>
+  );
+}
